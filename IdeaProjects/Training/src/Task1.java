6112754fddd7d54228bf18b2230184986a9d9fbc
vahram.mnatsakanyan <vahram.mnatsakanyan@picsart.com>
2021-03-15 00:21:06 +0400

    add Task1

---
+++ IdeaProjects/Training/src/Task1.java
@@ -0,0 +1,14 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("    - ");
+        System.out.println("   * *" );
+        System.out.println("  | | |");
+        System.out.println(" - - - -");
+        System.out.println("- Vahram -");
+        System.out.println(" - - - -");
+        System.out.println("  | | |");
+        System.out.println("   * *" );
+        System.out.println("    - ");
+
+    }
+}
