214142484b5d63a316964ff16388a37f05f2a94c
vahram.mnatsakanyan <vahram.mnatsakanyan@picsart.com>
2021-03-18 23:54:41 +0400

    Homework2 Task1

---
+++ src/Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+    public static void main(String[] args){
+        int [] myList ={12,44};
+        //myList[1] = 14;
+        //myList[2] = 43;
+        //System.out.println(myList);
+
+    }
+}
